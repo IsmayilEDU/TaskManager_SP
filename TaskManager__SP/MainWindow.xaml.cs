c4a6bf9f65a4ad914d99680ee9c2113fe9fd48fd
ismayilkerimzade <ismayil.programmer@gmail.com>
2023-10-28 19:26:32 +0400

    Finish project

---
+++ TaskManager__SP/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Diagnostics;
+using System.Linq;
+
+namespace TaskManager__SP
+{
+    public partial class MainWindow : Window
+    {
+        public List<Process> Processes { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Processes = new List<Process>();
+            Processes = Process.GetProcesses().ToList();
+        }
+
+        private void btn_CreateProcess_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (txtBox_ProcessName.Text == "") throw new Exception("Process Name can not be empty!");
+
+                var CreatedProcess = Process.Start(txtBox_ProcessName.Text.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_EndProcess_Click(object sender, RoutedEventArgs e)
+        {
+            Process SelectedProcess = listView_Processes.SelectedItem as Process;
+            SelectedProcess!.Kill();
+        }
+    }
+}
